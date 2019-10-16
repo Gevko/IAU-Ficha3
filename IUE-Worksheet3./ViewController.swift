a7f695b777feeb62dfcdc7ab526cccd7f543d77e
Oleksandr Gevko <2190272@c2-lcm-m06.local>
2019-10-16 20:42:21 +0100

    firsts commit

---
+++ IUE-Worksheet3./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IUE-Worksheet3.
+//
+//  Created by Oleksandr Gevko on 16/10/2019.
+//  Copyright © 2019 Oleksandr Gevko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
